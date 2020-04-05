2939d176461a0f0454077093324fb84734b63ef2
Yukti_Behal <55197733+Yukti1995@users.noreply.github.com>
2020-04-04 23:39:26 -0400

    Update mainwindow.h

--- Source_code/mainwindow.h
+++ Source_code/mainwindow.h
@@ -5,13 +5,16 @@ void recordMusic1()   //function to record music
         case QAudioRecorder::RecordingState://If it's already recording and you click button then it stops recording
             a_recorder1->stop();
             break;
+                
+                
         case QAudioRecorder::PausedState://otherwise if not recording and you click button then it starts recording
         case QAudioRecorder::StoppedState:
             QAudioEncoderSettings audioSettings;
+                
+                
+              
             audioSettings.setCodec("audio/amr");
             audioSettings.setQuality(QMultimedia::HighQuality);
-            a_recorder1->setEncodingSettings(audioSettings);
-            a_recorder1->setOutputLocation(QUrl::fromLocalFile("/home/pi/Music/song1.amr"));//address of file to be recorded
             a_recorder1->record();
             break;
         }
