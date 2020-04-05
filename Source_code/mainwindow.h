ef5e05e83c752c737d6dffd0282a10c4e95a532d
Yukti_Behal <55197733+Yukti1995@users.noreply.github.com>
2020-04-05 00:13:00 -0400

    Update mainwindow.h

--- Source_code/mainwindow.h
+++ Source_code/mainwindow.h
@@ -1,25 +1,25 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
-#include <QMediaPlayer> //Libraries to play music
-#include <QMediaPlaylist>
-#include <QAudioRecorder> //Library to rrecord audio
-#include "ui_mainwindow.h" 
+#include <QMainWindow>//Library to build main window 
+#include <QMediaPlayer> //Library to play music 
+#include <QMediaPlaylist>//Library to interface with other devices
+#include <QAudioRecorder> //Library to record music 
+#include "ui_mainwindow.h" //header to generate .ui file for home page QT5 wizard
 
-QAudioRecorder *a_recorder1 = nullptr;
-    QAudioRecorder *a_recorder2 = nullptr;//4 objects of audio rcorder for 4 recording buttons
-    QAudioRecorder *a_recorder3 = nullptr;
-    QAudioRecorder *a_recorder4 = nullptr;
+QAudioRecorder *a_recorder1 = nullptr;//first object for recording audio in four buttons 
+    QAudioRecorder *a_recorder2 = nullptr;//second
+    QAudioRecorder *a_recorder3 = nullptr;//third
+    QAudioRecorder *a_recorder4 = nullptr;//four
 
 
-void recordMusic1()   //function to record music
+void recordMusic1()   //function to record music in the first button 
     {
     
         switch (a_recorder1->state()) {
         case QAudioRecorder::RecordingState:  //If it's already recording and we click button then it stops recording
             a_recorder1->stop();
-            break;
+            break;// first object , it will automatically stop recording on pressing the button 
                 
                 
         case QAudioRecorder::PausedState://otherwise if not recording and you click button then it starts recording
