1ff75884696070e6c0d37ddcd2564bbc7272fcec
Yukti_Behal <55197733+Yukti1995@users.noreply.github.com>
2020-04-05 22:34:51 -0400

    Update mainwindow.cpp

--- Source_code/mainwindow.cpp
+++ Source_code/mainwindow.cpp
@@ -1,4 +1,15 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+
 a_recorder1 = new QAudioRecorder;
     a_recorder2 = new QAudioRecorder;
     a_recorder3 = new QAudioRecorder;
     a_recorder4 = new QAudioRecorder;
+ m_player1=new QMediaPlayer;
+    m_player2=new QMediaPlayer;
+    m_player3=new QMediaPlayer;
+    m_player4=new QMediaPlayer;
