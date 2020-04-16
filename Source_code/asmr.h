1a66dd7dee46b028debeff8410a80e12fa188dd1
Yukti_Behal <55197733+Yukti1995@users.noreply.github.com>
2020-04-16 17:08:42 -0400

    Update asmr.h

--- Source_code/asmr.h
+++ Source_code/asmr.h
@@ -1,49 +1,50 @@
-#ifndef ASMR2GO_H // These happen to be there by default 
-#define ASMR2GO_H
+#ifndef ASMR_H // Defined by default
+#define ASMR_H
 
 #include <QMainWindow>//Library to build main window
 #include <QMediaPlayer> //Library to play music
 #include <QMediaPlaylist>//Library to interface with other devices
 #include <QAudioRecorder> //Library to record music
-#include "ui_asmr2go.h" //header to generate .ui file for home page QT5 wizard generation
+#include "ui_asmr.h" //header to generate .ui file for home page QT5 wizard generation
 
 namespace Ui {
-class ASMR2Go;
+class ASMR;
 }
 
-class ASMR2Go : public QMainWindow // Links the program to the main GUI window 
+class ASMR : public QMainWindow // Links the program to the main GUI window 
 {
     Q_OBJECT
 
 public:
-    explicit ASMR2Go(QWidget *parent = nullptr);
-    ~ASMR2Go();
+    explicit ASMR(QWidget *parent = nullptr);
+    ~ASMR();
 
 private: //functions can be accessed only inside the class
-    Ui::ASMR2GO *ui;
-    QAudioRecorder *rec_1 = nullptr;
-    QAudioRecorder *rec_2 = nullptr;
-    QAudioRecorder *rec_3 = nullptr;
-    QAudioRecorder *rec_4 = nullptr;
+    Ui::ASMR *ui;
+    QAudioRecorder *a_recorder1 = nullptr;
+    QAudioRecorder *a_recorder2 = nullptr;
+    QAudioRecorder *a_recorder3 = nullptr;
+    QAudioRecorder *a_recorder4= nullptr;
     // 4 objects to process live recording on the system 
-    QMediaPlayer *play_1 = nullptr;
-    QMediaPlayer *play_2 = nullptr;
-    QMediaPlayer *play_3 = nullptr;
-    QMediaPlayer *play_4 = nullptr;
+    QMediaPlayer *m_player1 = nullptr;
+    QMediaPlayer *m_player2 = nullptr;
+    QMediaPlayer *m_player3 = nullptr;
+    QMediaPlayer *m_player4 = nullptr;
     //4 objects to process playing of 4 recorded voices 
-    QMediaPlayer *play_5 = nullptr;
-    QMediaPlayer *play_6 = nullptr;
-    QMediaPlayer *play_7 = nullptr;
-    QMediaPlayer *play_8 = nullptr;
+    QMediaPlayer *m_player5 = nullptr;
+    QMediaPlayer *m_player6= nullptr;
+    QMediaPlayer *m_player7= nullptr;
+    QMediaPlayer *m_player8 = nullptr;
+    // 4 objects to play the pre-recorded voices 
     //my main functions()
 
 private slots:
     void recordMusic1() // main function 1
     {
-        switch (rec_1->state()) {
+        switch (a_recorder1->state()) {
         case QAudioRecorder::RecordingState: // if the current state of the recorder function is already recording, this would stop it and 
                 //break it out of the loop
-            rec_1->stop();
+            a_recorder1->stop();
             break;
           
         case QAudioRecorder::PausedState://when the current state of recorder is paused, it is allowed to go in the stopped state and 
@@ -52,18 +53,18 @@ private slots:
             QAudioEncoderSettings audioSettings;
             audioSettings.setCodec("audio/amr");// amr , audio compression format for live speech signals 
             audioSettings.setQuality(QMultimedia::HighQuality);
-            rec_1->setEncodingSettings(audioSettings);
-            rec_1->setOutputLocation(QUrl::fromLocalFile("/home/max/song1.amr")); // this is the output location.
-            rec_1->record();
+            a_recorder1->setEncodingSettings(audioSettings);
+            a_recorder1->setOutputLocation(QUrl::fromLocalFile("/home/max/song1.amr")); // this is the output location.
+            a_recorder1->record();
             break;
             //
         }
     }
     void recordMusic2()
     {
-        switch (rec_2->state()) {
+        switch (a_recorder2->state()) {
         case QAudioRecorder::RecordingState:
-            rec_2->stop();
+            a_recorder2->stop();
             break;
             //
         case QAudioRecorder::PausedState:
@@ -71,18 +72,18 @@ private slots:
             QAudioEncoderSettings audioSettings;
             audioSettings.setCodec("audio/amr");
             audioSettings.setQuality(QMultimedia::HighQuality);
-            rec_2->setEncodingSettings(audioSettings);
-            rec_2->setOutputLocation(QUrl::fromLocalFile("/home/max/song2.amr"));
-            rec_2->record();
+            a_recorder2->setEncodingSettings(audioSettings);
+            a_recorder2->setOutputLocation(QUrl::fromLocalFile("/home/max/song2.amr"));
+            a_recorder2->record();
             break;
             //
         }
     }
     void recordMusic3()
     {
-        switch (rec_3->state()) {
+        switch (a_recorder3->state()) {
         case QAudioRecorder::RecordingState:
-            rec_3->stop();
+            a_recorder3->stop();
             break;
             //
         case QAudioRecorder::PausedState:
@@ -90,18 +91,18 @@ private slots:
             QAudioEncoderSettings audioSettings;
             audioSettings.setCodec("audio/amr");
             audioSettings.setQuality(QMultimedia::HighQuality);
-            rec_3->setEncodingSettings(audioSettings);
-            rec_3->setOutputLocation(QUrl::fromLocalFile("/home/max/song3.amr"));
-            rec_3->record();
+            a_recorder3->setEncodingSettings(audioSettings);
+            a_recorder3->setOutputLocation(QUrl::fromLocalFile("/home/max/song3.amr"));
+            a_recorder3->record();
             break;
             //
         }
     }
     void recordMusic4()
     {
-        switch (rec_4->state()) {
+        switch (a_recorder4->state()) {
         case QAudioRecorder::RecordingState:
-            rec_4->stop();
+            a_recorder4->stop();
             break;
             //
         case QAudioRecorder::PausedState:
@@ -109,9 +110,9 @@ private slots:
             QAudioEncoderSettings audioSettings;
             audioSettings.setCodec("audio/amr");
             audioSettings.setQuality(QMultimedia::HighQuality);
-            rec_4->setEncodingSettings(audioSettings);
-            rec_4->setOutputLocation(QUrl::fromLocalFile("/home/max/song4.amr"));
-            rec_4->record();
+            a_recorder4->setEncodingSettings(audioSettings);
+            a_recorder4->setOutputLocation(QUrl::fromLocalFile("/home/max/song4.amr"));
+            a_recorder4->record();
             break;
             //
         }
@@ -121,9 +122,9 @@ private slots:
 
     void playMusic1()
     {
-        switch (play_1->state()) {
+        switch (m_player1->state()) {
         case QMediaPlayer::PlayingState:
-            play_1->stop();
+            m_player1->stop();
             ui->pushbutton_1->setText("");
             break;
             //
@@ -132,8 +133,8 @@ private slots:
             QMediaPlaylist *playlist = new QMediaPlaylist();//to assign the voice recorded in rec1() object 
             playlist->addMedia(QUrl::fromLocalFile("/home/max/song1.amr")); //output where the object has to take extract it media from 
             playlist->setPlaybackMode(QMediaPlaylist::Loop); // playing the voice in a loop until the button is pressed
-            play_1 = new QMediaPlayer;// assigning playlist as a new media player object player to keep playing and avoiding device conncetion problems 
-            play_1->setPlaylist(playlist);
+            m_player1 = new QMediaPlayer;// assigning playlist as a new media player object player to keep playing and avoiding device conncetion problems 
+            m_player1->setPlaylist(playlist);
             ui->pushbutton_1->setText("...");
             play_1->play();
             break;
@@ -143,9 +144,9 @@ private slots:
 
     void playMusic2()
     {
-        switch (play_2->state()) {
+        switch (m_player2->state()) {
         case QMediaPlayer::PlayingState:
-            play_2->stop();
+            m_player2->stop();
             ui->pushbutton_2->setText("");
             break;
             //
@@ -154,10 +155,10 @@ private slots:
             QMediaPlaylist *playlist = new QMediaPlaylist();
             playlist->addMedia(QUrl::fromLocalFile("/home/max/song2.amr"));
             playlist->setPlaybackMode(QMediaPlaylist::Loop);
-            play_2 = new QMediaPlayer;
-            play_2->setPlaylist(playlist);
+            m_player2 = new QMediaPlayer;
+            m_player2->setPlaylist(playlist);
             ui->pushbutton_2->setText("...");
-            play_2->play();
+            m_player2->play();
             break;
             //
         }
@@ -165,9 +166,9 @@ private slots:
 
       void playMusic3()
     {
-        switch (play_3->state()) {
+        switch (m_player3->state()) {
         case QMediaPlayer::PlayingState:
-            play_3->stop();
+            m_player3->stop();
             ui->pushbutton_3->setText("");
             break;
             //
@@ -176,9 +177,9 @@ private slots:
             QMediaPlaylist *playlist = new QMediaPlaylist();
             playlist->addMedia(QUrl::fromLocalFile("/home/max/song3.amr"));
             playlist->setPlaybackMode(QMediaPlaylist::Loop);
-            play_3= new QMediaPlayer;
-            play_3->setPlaylist(playlist);
-            play_3->play();
+            m_player3= new QMediaPlayer;
+            m_player3->setPlaylist(playlist);
+            m_player3->play();
             ui->pushbutton_3->setText("...");
             break;
             //
@@ -187,9 +188,9 @@ private slots:
 
     void playMusic4()
     {
-        switch (play_4->state()) {
+        switch (m_player4->state()) {
         case QMediaPlayer::PlayingState:
-            play_4->stop();
+            m_player4->stop();
             ui->pushbutton_4->setText("");
             break;
             //
@@ -198,9 +199,9 @@ private slots:
             QMediaPlaylist *playlist = new QMediaPlaylist();
             playlist->addMedia(QUrl::fromLocalFile("/home/max/song4.amr"));
             playlist->setPlaybackMode(QMediaPlaylist::Loop);
-            play_4 = new QMediaPlayer;
-            play_4->setPlaylist(playlist);
-            play_4->play();
+            m_player4 = new QMediaPlayer;
+            m_player4->setPlaylist(playlist);
+            m_player4->play();
             ui->pushbutton_4->setText("...");
             break;
             //
@@ -209,10 +210,10 @@ private slots:
     }
     void playMusic5()
     {
-        switch (play_5->state()) {
+        switch (m_player5->state()) {
         case QMediaPlayer::PlayingState:
-            play_5->stop();
-            ui->pushbutton_5->setText("Cats Purr");
+            m_player3->stop();
+            ui->pushbutton_5->setText("1");
             break;
             //
         case QMediaPlayer::PausedState:
@@ -220,9 +221,9 @@ private slots:
             QMediaPlaylist *playlist = new QMediaPlaylist();
             playlist->addMedia(QUrl::fromLocalFile("/home/manush/Documents/yukti/audio_files/Catspurr.mp3"));
             playlist->setPlaybackMode(QMediaPlaylist::Loop);
-            play_5 = new QMediaPlayer;
-            play_5->setPlaylist(playlist);
-            play_5->play();
+            m_player5 = new QMediaPlayer;
+            m_player5->setPlaylist(playlist);
+            m_player5->play();
             ui->pushbutton_5->setText("Cats purr");
             break;
             //
@@ -230,10 +231,10 @@ private slots:
     }
         void playMusic6()
         {
-            switch (play_6->state()) {
+            switch (m_player6->state()) {
             case QMediaPlayer::PlayingState:
-                play_6->stop();
-                ui->pushbutton_6->setText("Leaves");
+                m_player6->stop();
+                ui->pushbutton_6->setText("2");
                 break;
                 //
             case QMediaPlayer::PausedState:
@@ -241,9 +242,9 @@ private slots:
                 QMediaPlaylist *playlist = new QMediaPlaylist();
                 playlist->addMedia(QUrl::fromLocalFile("/home/manush/Documents/yukti/audio_files/Leaves.mp3"));
                 playlist->setPlaybackMode(QMediaPlaylist::Loop);
-                play_6 = new QMediaPlayer;
-                play_6->setPlaylist(playlist);
-                play_6->play();
+                m_player6 = new QMediaPlayer;
+                m_player6->setPlaylist(playlist);
+                m_player6->play();
                 ui->pushbutton_6->setText("Leaves");
                 break;
                 //
@@ -252,10 +253,10 @@ private slots:
 
         void playMusic7()
         {
-            switch (play_7->state()) {
+            switch (m_player7->state()) {
             case QMediaPlayer::PlayingState:
-                play_7->stop();
-                ui->pushbutton_7->setText("Snowstorm");
+                m_player7->stop();
+                ui->pushbutton_7->setText("3");
                 break;
                 //
             case QMediaPlayer::PausedState:
@@ -263,9 +264,9 @@ private slots:
                 QMediaPlaylist *playlist = new QMediaPlaylist();
                 playlist->addMedia(QUrl::fromLocalFile("/home/manush/Documents/yukti/Final_Submission_ASMR2GO/Software/Sound_processing/Sound_samples/Snowstorm.mp3"));
                 playlist->setPlaybackMode(QMediaPlaylist::Loop);
-                play_7 = new QMediaPlayer;
-                play_7->setPlaylist(playlist);
-                play_7->play();
+                m_player7= new QMediaPlayer;
+                m_player7->setPlaylist(playlist);
+                m_player7->play();
                 ui->pushbutton_7->setText("Snowstorm");
                 break;
                 //
@@ -273,10 +274,10 @@ private slots:
         }
         void playMusic8()
         {
-            switch (play_8->state()) {
+            switch (m_player8->state()) {
             case QMediaPlayer::PlayingState:
-                play_8->stop();
-                ui->pushbutton_7->setText("Pebbles");
+                m_player8->stop();
+                ui->pushbutton_7->setText("4");
                 break;
                 //
             case QMediaPlayer::PausedState:
@@ -284,15 +285,15 @@ private slots:
                 QMediaPlaylist *playlist = new QMediaPlaylist();
                 playlist->addMedia(QUrl::fromLocalFile("/home/manush/Documents/yukti/Final_Submission_ASMR2GO/Software/Sound_processing/Sound_samples/Pebbles.mp3"));
                 playlist->setPlaybackMode(QMediaPlaylist::Loop);
-                play_8 = new QMediaPlayer;
-                play_8->setPlaylist(playlist);
-                play_8->play();
+                m_player8 = new QMediaPlayer;
+                m_player8->setPlaylist(playlist);
+                m_player8->play();
                 ui->pushbutton_8->setText("Pebbles");
                 break;
                 //
             }
         }
 
-#endif // ASMR2GO_H
+#endif // ASMR_H
 
 
