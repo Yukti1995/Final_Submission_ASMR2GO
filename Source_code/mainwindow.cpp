f2fefcae2456c50e60af658a10eaca78bef5129a
Yukti_Behal <55197733+Yukti1995@users.noreply.github.com>
2020-04-16 17:20:08 -0400

    Update mainwindow.cpp

--- Source_code/mainwindow.cpp
+++ Source_code/mainwindow.cpp
@@ -1,10 +1,10 @@
-#include "asmr2go.h"
+#include "asmr.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    ASMR2Go w;
+    ASMR w;
     w.show();
 
     return a.exec();
